5510f7df30546c083f0253702421510175c500ab
WeslleySturmann <weslley.w.sturmer@gmail.com>
2025-10-30 16:11:51 -0300

    Começo dos ajustes no footer da página, re-estilização e adição de algumas informações

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,7 @@
 import styles from './Footer.module.css'
 
+import logo from '../../img/logo.png'
+
 import { FaLinkedin, FaGithub, FaWhatsapp, FaGmail } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
@@ -8,14 +10,28 @@ import { Link } from 'react-router-dom'
 function Footer() {
     return (
         <footer className={styles.container}>
-            <h2>Minhas redes</h2>
-            <div>
-                <a href='https://www.linkedin.com/in/weslleywsturmer/' target='_blank' ><FaLinkedin /></a>
-                <a href='https://github.com/WeslleySturmer' target='_blank' ><FaGithub /></a>
-                <a href='https://wa.me/5549991031006' target='_blank' ><FaWhatsapp /></a>
-                <a href='mailto:weslley.w.sturmer@gmail.com' target='_blank' ><MdEmail /></a>
-            </div>
-            <p>© 2025 Finora. Todos os direitos reservados.</p>
+            <section>
+                <h2>Minhas redes</h2>
+                <div>
+                    <a href='https://www.linkedin.com/in/weslleywsturmer/' target='_blank' ><FaLinkedin /></a>
+                    <a href='https://github.com/WeslleySturmer' target='_blank' ><FaGithub /></a>
+                    <a href='https://wa.me/5549991031006' target='_blank' ><FaWhatsapp /></a>
+                    <a href='mailto:weslley.w.sturmer@gmail.com' target='_blank' ><MdEmail /></a>
+                </div>
+                <p>© 2025 Finora. Todos os direitos reservados.</p>
+            </section>
+
+            <section>
+                <nav>
+                    <Link to='/'><img src={logo} alt='Logo Finora' className={styles.logo} /></Link>
+                    <ul>
+                        <li> <Link to='/'>Home</Link> </li>
+                        <li> <Link to='/wallet'>Carteira</Link> </li>
+                        <li> <Link to='/dashboard'>Dashboard</Link> </li>
+                        <li> <Link to='/aboutus'>Sobre nós</Link> </li>
+                    </ul>
+                </nav>
+            </section>
         </footer>
     )
 }
